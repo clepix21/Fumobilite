abfa173cfd7f71a10d44204bbea6f690469d12ae
clepix21 <clement.lemaire2107@gmail.com>
2025-03-22 12:43:52 +0100

    Ajout de l'interface utilisateur pour UCRechercheItineraire
    Création du fichier `UCRechercheItineraire.Designer.cs` pour définir l'interface utilisateur avec des contrôles tels que des labels, des comboboxes et un DataGridView.
    
    Modification de `UCRechercheItineraire.cs` pour supprimer les initialisations de composants obsolètes et ajouter des initialisations de services pour gérer les arrêts, lignes et horaires.
    
    Ajout de validations pour garantir que les arrêts de départ et d'arrivée sont différents, ainsi que pour gérer les cas où aucun itinéraire n'est trouvé.
    
    Mise à jour de la méthode `ChargerArrets` pour configurer les comboboxes avec les arrêts disponibles.
    
    Ajout de commentaires pour clarifier le code et amélioration de la logique de sélection d'itinéraires dans le DataGridView.

--- Fumoblilite.Interface/UserControls/UCRechercheItineraire.cs
+++ Fumoblilite.Interface/UserControls/UCRechercheItineraire.cs
@@ -21,252 +21,18 @@ namespace Fumoblilite.Interface.UserControls
         {
             InitializeComponent();
             _connectionString = connectionString;
-            
+
             // Initialisation des repositories
             IRepositoryArret repositoryArret = new RepositoryArret(_connectionString);
             IRepositoryLigne repositoryLigne = new RepositoryLigne(_connectionString);
             IRepositoryArretLigne repositoryArretLigne = new RepositoryArretLigne(_connectionString, repositoryArret);
             IRepositoryHoraire repositoryHoraire = new RepositoryHoraire(_connectionString);
-            
+
             // Initialisation des services
             _serviceArret = new ServiceArret(repositoryArret);
             _serviceLigne = new ServiceLigne(repositoryLigne, repositoryArretLigne);
             _serviceItineraire = new ServiceItineraire(repositoryArret, repositoryLigne, repositoryArretLigne, repositoryHoraire);
         }
-
-        private void InitializeComponent()
-        {
-            this.lblTitre = new System.Windows.Forms.Label();
-            this.grpRecherche = new System.Windows.Forms.GroupBox();
-            this.lblArretDepart = new System.Windows.Forms.Label();
-            this.cboArretDepart = new System.Windows.Forms.ComboBox();
-            this.lblArretArrivee = new System.Windows.Forms.Label();
-            this.cboArretArrivee = new System.Windows.Forms.ComboBox();
-            this.lblDate = new System.Windows.Forms.Label();
-            this.dtpDate = new System.Windows.Forms.DateTimePicker();
-            this.lblHeure = new System.Windows.Forms.Label();
-            this.dtpHeure = new System.Windows.Forms.DateTimePicker();
-            this.rbDepart = new System.Windows.Forms.RadioButton();
-            this.rbArrivee = new System.Windows.Forms.RadioButton();
-            this.btnRechercher = new System.Windows.Forms.Button();
-            this.dgvItineraires = new System.Windows.Forms.DataGridView();
-            this.pnlDetailsItineraire = new System.Windows.Forms.Panel();
-            this.lblDetailsItineraire = new System.Windows.Forms.Label();
-            this.grpRecherche.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvItineraires)).BeginInit();
-            this.pnlDetailsItineraire.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // lblTitre
-            // 
-            this.lblTitre.AutoSize = true;
-            this.lblTitre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitre.Location = new System.Drawing.Point(15, 15);
-            this.lblTitre.Name = "lblTitre";
-            this.lblTitre.Size = new System.Drawing.Size(196, 24);
-            this.lblTitre.TabIndex = 0;
-            this.lblTitre.Text = "Recherche d\'itinéraire";
-            // 
-            // grpRecherche
-            // 
-            this.grpRecherche.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.grpRecherche.Controls.Add(this.btnRechercher);
-            this.grpRecherche.Controls.Add(this.rbArrivee);
-            this.grpRecherche.Controls.Add(this.rbDepart);
-            this.grpRecherche.Controls.Add(this.dtpHeure);
-            this.grpRecherche.Controls.Add(this.lblHeure);
-            this.grpRecherche.Controls.Add(this.dtpDate);
-            this.grpRecherche.Controls.Add(this.lblDate);
-            this.grpRecherche.Controls.Add(this.cboArretArrivee);
-            this.grpRecherche.Controls.Add(this.lblArretArrivee);
-            this.grpRecherche.Controls.Add(this.cboArretDepart);
-            this.grpRecherche.Controls.Add(this.lblArretDepart);
-            this.grpRecherche.Location = new System.Drawing.Point(15, 50);
-            this.grpRecherche.Name = "grpRecherche";
-            this.grpRecherche.Size = new System.Drawing.Size(770, 150);
-            this.grpRecherche.TabIndex = 1;
-            this.grpRecherche.TabStop = false;
-            this.grpRecherche.Text = "Critères de recherche";
-            // 
-            // lblArretDepart
-            // 
-            this.lblArretDepart.AutoSize = true;
-            this.lblArretDepart.Location = new System.Drawing.Point(20, 30);
-            this.lblArretDepart.Name = "lblArretDepart";
-            this.lblArretDepart.Size = new System.Drawing.Size(83, 13);
-            this.lblArretDepart.TabIndex = 0;
-            this.lblArretDepart.Text = "Arrêt de départ :";
-            // 
-            // cboArretDepart
-            // 
-            this.cboArretDepart.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboArretDepart.FormattingEnabled = true;
-            this.cboArretDepart.Location = new System.Drawing.Point(120, 27);
-            this.cboArretDepart.Name = "cboArretDepart";
-            this.cboArretDepart.Size = new System.Drawing.Size(250, 21);
-            this.cboArretDepart.TabIndex = 1;
-            // 
-            // lblArretArrivee
-            // 
-            this.lblArretArrivee.AutoSize = true;
-            this.lblArretArrivee.Location = new System.Drawing.Point(20, 60);
-            this.lblArretArrivee.Name = "lblArretArrivee";
-            this.lblArretArrivee.Size = new System.Drawing.Size(82, 13);
-            this.lblArretArrivee.TabIndex = 2;
-            this.lblArretArrivee.Text = "Arrêt d\'arrivée :";
-            // 
-            // cboArretArrivee
-            // 
-            this.cboArretArrivee.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cboArretArrivee.FormattingEnabled = true;
-            this.cboArretArrivee.Location = new System.Drawing.Point(120, 57);
-            this.cboArretArrivee.Name = "cboArretArrivee";
-            this.cboArretArrivee.Size = new System.Drawing.Size(250, 21);
-            this.cboArretArrivee.TabIndex = 3;
-            // 
-            // lblDate
-            // 
-            this.lblDate.AutoSize = true;
-            this.lblDate.Location = new System.Drawing.Point(400, 30);
-            this.lblDate.Name = "lblDate";
-            this.lblDate.Size = new System.Drawing.Size(36, 13);
-            this.lblDate.TabIndex = 4;
-            this.lblDate.Text = "Date :";
-            // 
-            // dtpDate
-            // 
-            this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtpDate.Location = new System.Drawing.Point(450, 27);
-            this.dtpDate.Name = "dtpDate";
-            this.dtpDate.Size = new System.Drawing.Size(120, 20);
-            this.dtpDate.TabIndex = 5;
-            // 
-            // lblHeure
-            // 
-            this.lblHeure.AutoSize = true;
-            this.lblHeure.Location = new System.Drawing.Point(400, 60);
-            this.lblHeure.Name = "lblHeure";
-            this.lblHeure.Size = new System.Drawing.Size(42, 13);
-            this.lblHeure.TabIndex = 6;
-            this.lblHeure.Text = "Heure :";
-            // 
-            // dtpHeure
-            // 
-            this.dtpHeure.CustomFormat = "HH:mm";
-            this.dtpHeure.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpHeure.Location = new System.Drawing.Point(450, 57);
-            this.dtpHeure.Name = "dtpHeure";
-            this.dtpHeure.ShowUpDown = true;
-            this.dtpHeure.Size = new System.Drawing.Size(120, 20);
-            this.dtpHeure.TabIndex = 7;
-            // 
-            // rbDepart
-            // 
-            this.rbDepart.AutoSize = true;
-            this.rbDepart.Checked = true;
-            this.rbDepart.Location = new System.Drawing.Point(120, 90);
-            this.rbDepart.Name = "rbDepart";
-            this.rbDepart.Size = new System.Drawing.Size(102, 17);
-            this.rbDepart.TabIndex = 8;
-            this.rbDepart.TabStop = true;
-            this.rbDepart.Text = "Heure de départ";
-            this.rbDepart.UseVisualStyleBackColor = true;
-            // 
-            // rbArrivee
-            // 
-            this.rbArrivee.AutoSize = true;
-            this.rbArrivee.Location = new System.Drawing.Point(250, 90);
-            this.rbArrivee.Name = "rbArrivee";
-            this.rbArrivee.Size = new System.Drawing.Size(101, 17);
-            this.rbArrivee.TabIndex = 9;
-            this.rbArrivee.Text = "Heure d\'arrivée";
-            this.rbArrivee.UseVisualStyleBackColor = true;
-            // 
-            // btnRechercher
-            // 
-            this.btnRechercher.Location = new System.Drawing.Point(450, 90);
-            this.btnRechercher.Name = "btnRechercher";
-            this.btnRechercher.Size = new System.Drawing.Size(120, 30);
-            this.btnRechercher.TabIndex = 10;
-            this.btnRechercher.Text = "Rechercher";
-            this.btnRechercher.UseVisualStyleBackColor = true;
-            this.btnRechercher.Click += new System.EventHandler(this.btnRechercher_Click);
-            // 
-            // dgvItineraires
-            // 
-            this.dgvItineraires.AllowUserToAddRows = false;
-            this.dgvItineraires.AllowUserToDeleteRows = false;
-            this.dgvItineraires.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.dgvItineraires.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dgvItineraires.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvItineraires.Location = new System.Drawing.Point(15, 210);
-            this.dgvItineraires.MultiSelect = false;
-            this.dgvItineraires.Name = "dgvItineraires";
-            this.dgvItineraires.ReadOnly = true;
-            this.dgvItineraires.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvItineraires.Size = new System.Drawing.Size(400, 225);
-            this.dgvItineraires.TabIndex = 2;
-            this.dgvItineraires.SelectionChanged += new System.EventHandler(this.dgvItineraires_SelectionChanged);
-            // 
-            // pnlDetailsItineraire
-            // 
-            this.pnlDetailsItineraire.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pnlDetailsItineraire.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pnlDetailsItineraire.Controls.Add(this.lblDetailsItineraire);
-            this.pnlDetailsItineraire.Location = new System.Drawing.Point(430, 210);
-            this.pnlDetailsItineraire.Name = "pnlDetailsItineraire";
-            this.pnlDetailsItineraire.Size = new System.Drawing.Size(355, 225);
-            this.pnlDetailsItineraire.TabIndex = 3;
-            // 
-            // lblDetailsItineraire
-            // 
-            this.lblDetailsItineraire.AutoSize = true;
-            this.lblDetailsItineraire.Location = new System.Drawing.Point(10, 10);
-            this.lblDetailsItineraire.Name = "lblDetailsItineraire";
-            this.lblDetailsItineraire.Size = new System.Drawing.Size(174, 13);
-            this.lblDetailsItineraire.TabIndex = 0;
-            this.lblDetailsItineraire.Text = "Sélectionnez un itinéraire à gauche";
-            // 
-            // UCRechercheItineraire
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.pnlDetailsItineraire);
-            this.Controls.Add(this.dgvItineraires);
-            this.Controls.Add(this.grpRecherche);
-            this.Controls.Add(this.lblTitre);
-            this.Name = "UCRechercheItineraire";
-            this.Size = new System.Drawing.Size(800, 450);
-            this.Load += new System.EventHandler(this.UCRechercheItineraire_Load);
-            this.grpRecherche.ResumeLayout(false);
-            this.grpRecherche.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvItineraires)).EndInit();
-            this.pnlDetailsItineraire.ResumeLayout(false);
-            this.pnlDetailsItineraire.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-        }
-
-        private System.Windows.Forms.Label lblTitre;
-        private System.Windows.Forms.GroupBox grpRecherche;
-        private System.Windows.Forms.Label lblArretDepart;
-        private System.Windows.Forms.ComboBox cboArretDepart;
-        private System.Windows.Forms.Label lblArretArrivee;
-        private System.Windows.Forms.ComboBox cboArretArrivee;
-        private System.Windows.Forms.Label lblDate;
-        private System.Windows.Forms.DateTimePicker dtpDate;
-        private System.Windows.Forms.Label lblHeure;
-        private System.Windows.Forms.DateTimePicker dtpHeure;
-        private System.Windows.Forms.RadioButton rbDepart;
-        private System.Windows.Forms.RadioButton rbArrivee;
-        private System.Windows.Forms.Button btnRechercher;
-        private System.Windows.Forms.DataGridView dgvItineraires;
-        private System.Windows.Forms.Panel pnlDetailsItineraire;
-        private System.Windows.Forms.Label lblDetailsItineraire;
 
         private void UCRechercheItineraire_Load(object sender, EventArgs e)
         {
@@ -280,17 +46,17 @@ namespace Fumoblilite.Interface.UserControls
             try
             {
                 List<Arret> arrets = _serviceArret.ObtenirTous();
-                
+
                 // Configuration du ComboBox de départ
                 cboArretDepart.DisplayMember = "Nom";
                 cboArretDepart.ValueMember = "Id";
                 cboArretDepart.DataSource = new List<Arret>(arrets);
-                
+
                 // Configuration du ComboBox d'arrivée
                 cboArretArrivee.DisplayMember = "Nom";
                 cboArretArrivee.ValueMember = "Id";
                 cboArretArrivee.DataSource = new List<Arret>(arrets);
-                
+
                 if (arrets.Count > 1)
                 {
                     cboArretArrivee.SelectedIndex = 1; // Sélectionner le deuxième arrêt par défaut
@@ -314,7 +80,7 @@ namespace Fumoblilite.Interface.UserControls
 
                 int arretDepartId = (int)cboArretDepart.SelectedValue;
                 int arretArriveeId = (int)cboArretArrivee.SelectedValue;
-                
+
                 if (arretDepartId == arretArriveeId)
                 {
                     MessageBox.Show("Les arrêts de départ et d'arrivée doivent être différents.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning);
@@ -327,7 +93,7 @@ namespace Fumoblilite.Interface.UserControls
 
                 // Rechercher les itinéraires
                 List<Itineraire> itineraires = _serviceItineraire.RechercherItineraires(arretDepartId, arretArriveeId, dateHeure, estHeureDepart);
-                
+
                 if (itineraires.Count == 0)
                 {
                     MessageBox.Show("Aucun itinéraire trouvé pour les critères spécifiés.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -347,10 +113,10 @@ namespace Fumoblilite.Interface.UserControls
                 }).ToList();
 
                 dgvItineraires.DataSource = itinerairesAffichage;
-                
+
                 // Masquer la colonne de l'objet complet
                 dgvItineraires.Columns["ItineraireComplet"].Visible = false;
-                
+
                 // Sélectionner le premier itinéraire
                 if (dgvItineraires.Rows.Count > 0)
                 {
@@ -483,4 +249,3 @@ namespace Fumoblilite.Interface.UserControls
         }
     }
 }
-
