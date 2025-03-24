7a7b6ee1cee0b24e3307488f54ced531ebc26135
clepix21 <clement.lemaire2107@gmail.com>
2025-03-24 21:46:33 +0100

    commentaire 2

--- Fumoblilite.Interface/Forms/FormPrincipal.cs
+++ Fumoblilite.Interface/Forms/FormPrincipal.cs
@@ -20,14 +20,17 @@ namespace Fumoblilite.Interface.Forms
 
         private void FormPrincipal_Load(object sender, EventArgs e)
         {
+            // Vérifier si un utilisateur est connecté
             if (_utilisateurConnecte != null)
             {
+                // Afficher les informations de l'utilisateur connecté
                 lblStatusUtilisateur.Text = $"Utilisateur connecté: {_utilisateurConnecte.Prenom} {_utilisateurConnecte.Nom} ({_utilisateurConnecte.Role})";
                 menuItemDeconnexion.Visible = true;
                 menuItemConnexion.Visible = false; // Masquer le bouton de connexion
             }
             else
             {
+                // Mode invité
                 lblStatusUtilisateur.Text = "Mode invité";
                 menuItemDeconnexion.Visible = false;
                 menuItemConnexion.Visible = true; // Afficher le bouton de connexion
@@ -43,26 +46,30 @@ namespace Fumoblilite.Interface.Forms
                 menuGestion.Visible = false;
             }
 
+            // Afficher le réseau par défaut
             AfficherReseau();
         }
 
         private void FormPrincipal_FormClosing(object sender, FormClosingEventArgs e)
         {
+            // Demander confirmation avant de fermer l'application
             if (e.CloseReason == CloseReason.UserClosing)
             {
                 var message = MessageBox.Show("Êtes-vous sûr de vouloir quitter?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                if (message == DialogResult.No) {
-                    e.Cancel = true;
+                if (message == DialogResult.No)
+                {
+                    e.Cancel = true; // Annuler la fermeture
                 }
-                else {
-                    Application.Exit();
+                else
+                {
+                    Application.Exit(); // Fermer l'application
                 }
             }
         }
 
-
         private void AfficherReseau()
         {
+            // Afficher le contrôle utilisateur de consultation du réseau
             panelContenu.Controls.Clear();
             UCConsultationReseau ucConsultationReseau = new UCConsultationReseau(_connectionString);
             ucConsultationReseau.Dock = DockStyle.Fill;
@@ -71,9 +78,11 @@ namespace Fumoblilite.Interface.Forms
 
         private void menuItemConnexion_Click(object sender, EventArgs e)
         {
+            // Ouvrir le formulaire de connexion
             FormConnexion formConnexion = new FormConnexion(_connectionString);
             formConnexion.FormClosed += (s, args) =>
             {
+                // Si un utilisateur s'est connecté, ouvrir une nouvelle instance de FormPrincipal
                 if (formConnexion.UtilisateurConnecte != null)
                 {
                     this.Hide();
@@ -86,6 +95,7 @@ namespace Fumoblilite.Interface.Forms
 
         private void menuItemDeconnexion_Click(object sender, EventArgs e)
         {
+            // Déconnexion de l'utilisateur et ouverture d'une nouvelle instance de FormPrincipal en mode invité
             this.Hide();
             FormPrincipal formPrincipal = new FormPrincipal(_connectionString, null);
             formPrincipal.Show();
@@ -93,51 +103,61 @@ namespace Fumoblilite.Interface.Forms
 
         private void menuItemQuitter_Click(object sender, EventArgs e)
         {
+            // Quitter l'application
             Application.Exit();
         }
 
         private void menuItemArrets_Click(object sender, EventArgs e)
         {
+            // Charger le contrôle utilisateur de gestion des arrêts
             ChargerUserControl(new UCGestionArrets(_connectionString));
         }
 
         private void menuItemLignes_Click(object sender, EventArgs e)
         {
+            // Charger le contrôle utilisateur de gestion des lignes
             ChargerUserControl(new UCGestionLignes(_connectionString));
         }
 
         private void menuItemHoraires_Click(object sender, EventArgs e)
         {
+            // Charger le contrôle utilisateur de gestion des horaires
             ChargerUserControl(new UCGestionHoraires(_connectionString));
         }
 
         private void menuItemUtilisateurs_Click(object sender, EventArgs e)
         {
+            // Charger le contrôle utilisateur de gestion des utilisateurs
             ChargerUserControl(new UCGestionUtilisateurs(_connectionString));
         }
 
         private void menuItemReseau_Click(object sender, EventArgs e)
         {
+            // Charger le contrôle utilisateur de consultation du réseau
             ChargerUserControl(new UCConsultationReseau(_connectionString));
         }
 
         private void menuItemLigneDetails_Click(object sender, EventArgs e)
         {
+            // Charger le contrôle utilisateur de consultation des détails de ligne
             ChargerUserControl(new UCConsultationLigne(_connectionString));
         }
 
         private void menuItemHorairesJournee_Click(object sender, EventArgs e)
         {
+            // Charger le contrôle utilisateur de consultation des horaires de la journée
             ChargerUserControl(new UCConsultationHoraires(_connectionString));
         }
 
         private void menuItemItineraire_Click(object sender, EventArgs e)
         {
+            // Charger le contrôle utilisateur de recherche d'itinéraire
             ChargerUserControl(new UCRechercheItineraire(_connectionString));
         }
 
         private void ChargerUserControl(UserControl uc)
         {
+            // Charger un contrôle utilisateur dans le panneau de contenu
             panelContenu.Controls.Clear();
             uc.Dock = DockStyle.Fill;
             panelContenu.Controls.Add(uc);
