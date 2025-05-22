c7ed4c2416ce8c8323bec20a3736912b4b9297a5
clepix21 <clement.lemaire2107@gmail.com>
2025-05-22 23:24:10 +0200

    Ajout de l'interface UCCredits et de nouvelles ressources
    Modifications apportées au fichier `Fumoblilite.Interface.csproj` pour inclure un sous-type pour `UCCredits.cs` et ajouter une ressource intégrée `UCCredits.resx`.
    
    Dans `UCCredits.Designer.cs`, ajout de deux nouvelles étiquettes (`label1` et `label2`) pour afficher des membres supplémentaires, avec des ajustements de dimensions et de positions des contrôles pour améliorer l'interface.
    
    Les propriétés de mise en page des panneaux et des boutons ont été modifiées pour s'adapter aux nouveaux éléments, avec des marges ajustées pour un meilleur espacement.
    
    Création d'un fichier de ressources `UCCredits.resx` pour gérer les chaînes localisées et autres ressources graphiques.

--- Fumoblilite.Interface/UserControls/UCCredits.Designer.cs
+++ Fumoblilite.Interface/UserControls/UCCredits.Designer.cs
@@ -42,6 +42,8 @@ namespace Fumoblilite.Interface.UserControls
             this.lblVersion = new System.Windows.Forms.Label();
             this.lblAppInfo = new System.Windows.Forms.Label();
             this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panelHeader.SuspendLayout();
             this.panelContent.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
@@ -53,8 +55,9 @@ namespace Fumoblilite.Interface.UserControls
             this.panelHeader.Controls.Add(this.lblTitle);
             this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelHeader.Name = "panelHeader";
-            this.panelHeader.Size = new System.Drawing.Size(450, 60);
+            this.panelHeader.Size = new System.Drawing.Size(600, 74);
             this.panelHeader.TabIndex = 0;
             // 
             // lblTitle
@@ -62,15 +65,18 @@ namespace Fumoblilite.Interface.UserControls
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitle.ForeColor = System.Drawing.Color.White;
-            this.lblTitle.Location = new System.Drawing.Point(15, 15);
+            this.lblTitle.Location = new System.Drawing.Point(20, 18);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(242, 30);
+            this.lblTitle.Size = new System.Drawing.Size(315, 37);
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "Crédits de l\'application";
             // 
             // panelContent
             // 
             this.panelContent.BackColor = System.Drawing.Color.White;
+            this.panelContent.Controls.Add(this.label2);
+            this.panelContent.Controls.Add(this.label1);
             this.panelContent.Controls.Add(this.btnFermer);
             this.panelContent.Controls.Add(this.lblAnnee);
             this.panelContent.Controls.Add(this.lblMembre4);
@@ -83,10 +89,11 @@ namespace Fumoblilite.Interface.UserControls
             this.panelContent.Controls.Add(this.lblAppInfo);
             this.panelContent.Controls.Add(this.pictureBox);
             this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelContent.Location = new System.Drawing.Point(0, 60);
+            this.panelContent.Location = new System.Drawing.Point(0, 74);
+            this.panelContent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelContent.Name = "panelContent";
-            this.panelContent.Padding = new System.Windows.Forms.Padding(20);
-            this.panelContent.Size = new System.Drawing.Size(450, 290);
+            this.panelContent.Padding = new System.Windows.Forms.Padding(27, 25, 27, 25);
+            this.panelContent.Size = new System.Drawing.Size(600, 357);
             this.panelContent.TabIndex = 1;
             // 
             // btnFermer
@@ -96,9 +103,10 @@ namespace Fumoblilite.Interface.UserControls
             this.btnFermer.FlatAppearance.BorderSize = 0;
             this.btnFermer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnFermer.ForeColor = System.Drawing.Color.White;
-            this.btnFermer.Location = new System.Drawing.Point(327, 240);
+            this.btnFermer.Location = new System.Drawing.Point(436, 295);
+            this.btnFermer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnFermer.Name = "btnFermer";
-            this.btnFermer.Size = new System.Drawing.Size(100, 30);
+            this.btnFermer.Size = new System.Drawing.Size(133, 37);
             this.btnFermer.TabIndex = 10;
             this.btnFermer.Text = "Fermer";
             this.btnFermer.UseVisualStyleBackColor = false;
@@ -109,9 +117,10 @@ namespace Fumoblilite.Interface.UserControls
             this.lblAnnee.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.lblAnnee.AutoSize = true;
             this.lblAnnee.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAnnee.Location = new System.Drawing.Point(20, 250);
+            this.lblAnnee.Location = new System.Drawing.Point(27, 308);
+            this.lblAnnee.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblAnnee.Name = "lblAnnee";
-            this.lblAnnee.Size = new System.Drawing.Size(61, 15);
+            this.lblAnnee.Size = new System.Drawing.Size(58, 20);
             this.lblAnnee.TabIndex = 9;
             this.lblAnnee.Text = "© 2025";
             // 
@@ -119,9 +128,10 @@ namespace Fumoblilite.Interface.UserControls
             // 
             this.lblMembre4.AutoSize = true;
             this.lblMembre4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMembre4.Location = new System.Drawing.Point(40, 180);
+            this.lblMembre4.Location = new System.Drawing.Point(53, 222);
+            this.lblMembre4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMembre4.Name = "lblMembre4";
-            this.lblMembre4.Size = new System.Drawing.Size(70, 15);
+            this.lblMembre4.Size = new System.Drawing.Size(87, 20);
             this.lblMembre4.TabIndex = 8;
             this.lblMembre4.Text = "• Membre 4";
             // 
@@ -129,9 +139,10 @@ namespace Fumoblilite.Interface.UserControls
             // 
             this.lblMembre3.AutoSize = true;
             this.lblMembre3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMembre3.Location = new System.Drawing.Point(40, 160);
+            this.lblMembre3.Location = new System.Drawing.Point(53, 197);
+            this.lblMembre3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMembre3.Name = "lblMembre3";
-            this.lblMembre3.Size = new System.Drawing.Size(70, 15);
+            this.lblMembre3.Size = new System.Drawing.Size(87, 20);
             this.lblMembre3.TabIndex = 7;
             this.lblMembre3.Text = "• Membre 3";
             // 
@@ -139,9 +150,10 @@ namespace Fumoblilite.Interface.UserControls
             // 
             this.lblMembre2.AutoSize = true;
             this.lblMembre2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMembre2.Location = new System.Drawing.Point(40, 140);
+            this.lblMembre2.Location = new System.Drawing.Point(53, 172);
+            this.lblMembre2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMembre2.Name = "lblMembre2";
-            this.lblMembre2.Size = new System.Drawing.Size(70, 15);
+            this.lblMembre2.Size = new System.Drawing.Size(87, 20);
             this.lblMembre2.TabIndex = 6;
             this.lblMembre2.Text = "• Membre 2";
             // 
@@ -149,9 +161,10 @@ namespace Fumoblilite.Interface.UserControls
             // 
             this.lblMembre1.AutoSize = true;
             this.lblMembre1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMembre1.Location = new System.Drawing.Point(40, 120);
+            this.lblMembre1.Location = new System.Drawing.Point(53, 148);
+            this.lblMembre1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMembre1.Name = "lblMembre1";
-            this.lblMembre1.Size = new System.Drawing.Size(70, 15);
+            this.lblMembre1.Size = new System.Drawing.Size(87, 20);
             this.lblMembre1.TabIndex = 5;
             this.lblMembre1.Text = "• Membre 1";
             // 
@@ -159,9 +172,10 @@ namespace Fumoblilite.Interface.UserControls
             // 
             this.lblEquipe.AutoSize = true;
             this.lblEquipe.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEquipe.Location = new System.Drawing.Point(20, 100);
+            this.lblEquipe.Location = new System.Drawing.Point(27, 123);
+            this.lblEquipe.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblEquipe.Name = "lblEquipe";
-            this.lblEquipe.Size = new System.Drawing.Size(147, 19);
+            this.lblEquipe.Size = new System.Drawing.Size(190, 23);
             this.lblEquipe.TabIndex = 4;
             this.lblEquipe.Text = "Membres de l\'équipe :";
             // 
@@ -170,9 +184,10 @@ namespace Fumoblilite.Interface.UserControls
             this.lblGroupe.AutoSize = true;
             this.lblGroupe.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblGroupe.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
-            this.lblGroupe.Location = new System.Drawing.Point(175, 70);
+            this.lblGroupe.Location = new System.Drawing.Point(233, 86);
+            this.lblGroupe.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblGroupe.Name = "lblGroupe";
-            this.lblGroupe.Size = new System.Drawing.Size(100, 21);
+            this.lblGroupe.Size = new System.Drawing.Size(133, 28);
             this.lblGroupe.TabIndex = 3;
             this.lblGroupe.Text = "Groupe A-06";
             // 
@@ -180,9 +195,10 @@ namespace Fumoblilite.Interface.UserControls
             // 
             this.lblVersion.AutoSize = true;
             this.lblVersion.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblVersion.Location = new System.Drawing.Point(175, 50);
+            this.lblVersion.Location = new System.Drawing.Point(233, 62);
+            this.lblVersion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(80, 19);
+            this.lblVersion.Size = new System.Drawing.Size(93, 23);
             this.lblVersion.TabIndex = 2;
             this.lblVersion.Text = "Version 1.0";
             // 
@@ -191,29 +207,54 @@ namespace Fumoblilite.Interface.UserControls
             this.lblAppInfo.AutoSize = true;
             this.lblAppInfo.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAppInfo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
-            this.lblAppInfo.Location = new System.Drawing.Point(175, 20);
+            this.lblAppInfo.Location = new System.Drawing.Point(233, 25);
+            this.lblAppInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblAppInfo.Name = "lblAppInfo";
-            this.lblAppInfo.Size = new System.Drawing.Size(104, 25);
+            this.lblAppInfo.Size = new System.Drawing.Size(136, 32);
             this.lblAppInfo.TabIndex = 1;
             this.lblAppInfo.Text = "Fumobilité";
             // 
             // pictureBox
             // 
-            this.pictureBox.Location = new System.Drawing.Point(20, 20);
+            this.pictureBox.Location = new System.Drawing.Point(27, 25);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(130, 70);
+            this.pictureBox.Size = new System.Drawing.Size(173, 86);
             this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox.TabIndex = 0;
             this.pictureBox.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 257);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "• Membre 5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(203, 222);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "• Membre 6";
+            // 
             // UCCredits
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panelContent);
             this.Controls.Add(this.panelHeader);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "UCCredits";
-            this.Size = new System.Drawing.Size(450, 350);
+            this.Size = new System.Drawing.Size(600, 431);
             this.Load += new System.EventHandler(this.UCCredits_Load);
             this.panelHeader.ResumeLayout(false);
             this.panelHeader.PerformLayout();
@@ -240,5 +281,7 @@ namespace Fumoblilite.Interface.UserControls
         private System.Windows.Forms.Label lblMembre4;
         private System.Windows.Forms.Label lblAnnee;
         private System.Windows.Forms.Button btnFermer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
