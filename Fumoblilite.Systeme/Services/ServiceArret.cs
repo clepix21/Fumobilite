80932d61c4419d5d4f64706e93275495b2ab1a74
clepix21 <clement.lemaire2107@gmail.com>
2025-06-06 13:13:23 +0200

    Ajout de documentation et validation dans ServiceArret
    Ajout de la documentation XML pour la classe `ServiceArret` et ses méthodes.
    Intégration de `List<Arret>` pour les méthodes retournant des listes d'arrêts.
    Ajout de vérifications d'argument pour s'assurer que le nom de l'arrêt n'est pas vide ou nul lors de l'ajout ou de la modification d'un arrêt.

--- Fumoblilite.Systeme/Services/ServiceArret.cs
+++ Fumoblilite.Systeme/Services/ServiceArret.cs
@@ -5,30 +5,60 @@ using Fumoblilite.Systeme.Interfaces;
 
 namespace Fumoblilite.Systeme.Services
 {
+    /// <summary>
+    /// Service pour la gestion des arrêts.
+    /// </summary>
     public class ServiceArret
     {
+        /// <summary>
+        /// Référence au dépôt d'arrêts.
+        /// </summary>
         private readonly IRepositoryArret _repositoryArret;
 
+        /// <summary>
+        /// Initialise une nouvelle instance de la classe <see cref="ServiceArret"/>.
+        /// </summary>
+        /// <param name="repositoryArret">Le dépôt d'arrêts à utiliser.</param>
         public ServiceArret(IRepositoryArret repositoryArret)
         {
             _repositoryArret = repositoryArret;
         }
 
+        /// <summary>
+        /// Récupère la liste de tous les arrêts.
+        /// </summary>
+        /// <returns>Liste de tous les arrêts.</returns>
         public List<Arret> ObtenirTous()
         {
             return _repositoryArret.ObtenirTous();
         }
 
+        /// <summary>
+        /// Récupère un arrêt par son identifiant.
+        /// </summary>
+        /// <param name="id">Identifiant de l'arrêt.</param>
+        /// <returns>L'arrêt correspondant ou null si non trouvé.</returns>
         public Arret ObtenirParId(int id)
         {
             return _repositoryArret.ObtenirParId(id);
         }
 
+        /// <summary>
+        /// Récupère la liste des arrêts pour une ligne donnée.
+        /// </summary>
+        /// <param name="ligneId">Identifiant de la ligne.</param>
+        /// <returns>Liste des arrêts de la ligne.</returns>
         public List<Arret> ObtenirParLigne(int ligneId)
         {
             return _repositoryArret.ObtenirParLigne(ligneId);
         }
 
+        /// <summary>
+        /// Ajoute un nouvel arrêt.
+        /// </summary>
+        /// <param name="arret">L'arrêt à ajouter.</param>
+        /// <returns>L'identifiant de l'arrêt ajouté.</returns>
+        /// <exception cref="ArgumentException">Si le nom de l'arrêt est vide ou nul.</exception>
         public int Ajouter(Arret arret)
         {
             if (string.IsNullOrEmpty(arret.Nom))
@@ -37,6 +67,12 @@ namespace Fumoblilite.Systeme.Services
             return _repositoryArret.Ajouter(arret);
         }
 
+        /// <summary>
+        /// Modifie un arrêt existant.
+        /// </summary>
+        /// <param name="arret">L'arrêt à modifier.</param>
+        /// <returns>True si la modification a réussi, sinon false.</returns>
+        /// <exception cref="ArgumentException">Si le nom de l'arrêt est vide ou nul.</exception>
         public bool Modifier(Arret arret)
         {
             if (string.IsNullOrEmpty(arret.Nom))
@@ -46,6 +82,11 @@ namespace Fumoblilite.Systeme.Services
             return _repositoryArret.Modifier(arret);
         }
 
+        /// <summary>
+        /// Supprime un arrêt par son identifiant.
+        /// </summary>
+        /// <param name="id">Identifiant de l'arrêt à supprimer.</param>
+        /// <returns>True si la suppression a réussi, sinon false.</returns>
         public bool Supprimer(int id)
         {
             return _repositoryArret.Supprimer(id);
