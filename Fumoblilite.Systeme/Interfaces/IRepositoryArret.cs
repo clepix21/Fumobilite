47aec395068b279d944f3f7dec4add7f09bb0663
clepix21 <clement.lemaire2107@gmail.com>
2025-06-06 13:38:51 +0200

    Ajout de documentation XML pour les interfaces
    Ajout de la documentation XML pour les interfaces `IRepositoryArret` et `IRepositoryArretLigne`. Cela inclut des résumés et des descriptions des paramètres et des valeurs de retour pour chaque méthode, améliorant ainsi la lisibilité et la compréhension du code.

--- Fumoblilite.Systeme/Interfaces/IRepositoryArret.cs
+++ Fumoblilite.Systeme/Interfaces/IRepositoryArret.cs
@@ -3,13 +3,50 @@ using Fumoblilite.Systeme.Modeles;
 
 namespace Fumoblilite.Systeme.Interfaces
 {
+    /// <summary>
+    /// Interface pour la gestion des opérations CRUD sur les arrêts.
+    /// </summary>
     public interface IRepositoryArret
     {
+        /// <summary>
+        /// Récupère la liste de tous les arrêts.
+        /// </summary>
+        /// <returns>Liste de tous les arrêts.</returns>
         List<Arret> ObtenirTous();
+
+        /// <summary>
+        /// Récupère un arrêt par son identifiant.
+        /// </summary>
+        /// <param name="id">Identifiant de l'arrêt.</param>
+        /// <returns>L'arrêt correspondant à l'identifiant, ou null si non trouvé.</returns>
         Arret ObtenirParId(int id);
+
+        /// <summary>
+        /// Récupère la liste des arrêts associés à une ligne donnée.
+        /// </summary>
+        /// <param name="ligneId">Identifiant de la ligne.</param>
+        /// <returns>Liste des arrêts de la ligne spécifiée.</returns>
         List<Arret> ObtenirParLigne(int ligneId);
+
+        /// <summary>
+        /// Ajoute un nouvel arrêt.
+        /// </summary>
+        /// <param name="arret">L'arrêt à ajouter.</param>
+        /// <returns>L'identifiant de l'arrêt ajouté.</returns>
         int Ajouter(Arret arret);
+
+        /// <summary>
+        /// Modifie un arrêt existant.
+        /// </summary>
+        /// <param name="arret">L'arrêt à modifier.</param>
+        /// <returns>True si la modification a réussi, sinon false.</returns>
         bool Modifier(Arret arret);
+
+        /// <summary>
+        /// Supprime un arrêt par son identifiant.
+        /// </summary>
+        /// <param name="id">Identifiant de l'arrêt à supprimer.</param>
+        /// <returns>True si la suppression a réussi, sinon false.</returns>
         bool Supprimer(int id);
     }
 }
--- Fumoblilite.Systeme/Interfaces/IRepositoryArretLigne.cs
+++ Fumoblilite.Systeme/Interfaces/IRepositoryArretLigne.cs
@@ -3,12 +3,44 @@ using Fumoblilite.Systeme.Modeles;
 
 namespace Fumoblilite.Systeme.Interfaces
 {
+    /// <summary>
+    /// Interface pour la gestion des opérations sur les arrêts de ligne.
+    /// </summary>
     public interface IRepositoryArretLigne
     {
+        /// <summary>
+        /// Récupère la liste des arrêts de ligne pour une ligne donnée.
+        /// </summary>
+        /// <param name="ligneId">Identifiant de la ligne.</param>
+        /// <returns>Liste des arrêts de ligne associés à la ligne.</returns>
         List<ArretLigne> ObtenirParLigne(int ligneId);
+
+        /// <summary>
+        /// Récupère un arrêt de ligne par son identifiant.
+        /// </summary>
+        /// <param name="id">Identifiant de l'arrêt de ligne.</param>
+        /// <returns>L'arrêt de ligne correspondant ou null si non trouvé.</returns>
         ArretLigne ObtenirParId(int id);
+
+        /// <summary>
+        /// Ajoute un nouvel arrêt de ligne.
+        /// </summary>
+        /// <param name="arretLigne">L'arrêt de ligne à ajouter.</param>
+        /// <returns>L'identifiant de l'arrêt de ligne ajouté.</returns>
         int Ajouter(ArretLigne arretLigne);
+
+        /// <summary>
+        /// Modifie un arrêt de ligne existant.
+        /// </summary>
+        /// <param name="arretLigne">L'arrêt de ligne à modifier.</param>
+        /// <returns>True si la modification a réussi, sinon false.</returns>
         bool Modifier(ArretLigne arretLigne);
+
+        /// <summary>
+        /// Supprime un arrêt de ligne par son identifiant.
+        /// </summary>
+        /// <param name="id">Identifiant de l'arrêt de ligne à supprimer.</param>
+        /// <returns>True si la suppression a réussi, sinon false.</returns>
         bool Supprimer(int id);
     }
 }
