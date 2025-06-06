3f5a6734f67997eefb74678181d9f36cb5b4bebe
clepix21 <clement.lemaire2107@gmail.com>
2025-06-06 13:05:07 +0200

    Améliorations de l'interface et ajout de fonctionnalités
    Modifications apportées au fichier `FormConnexion.Designer.cs` pour améliorer le style et le positionnement des éléments de l'interface utilisateur, y compris les polices, les couleurs et les tailles des contrôles.
    
    Ajout de sous-types pour les ressources intégrées dans `Fumoblilite.Interface.csproj` afin d'optimiser la gestion des ressources.
    
    Ajout de nouvelles méthodes dans `ServiceLigne.cs` pour gérer les lignes et leurs arrêts, avec documentation XML pour améliorer la lisibilité et la maintenabilité du code.

--- Fumoblilite.Interface/Forms/FormConnexion.Designer.cs
+++ Fumoblilite.Interface/Forms/FormConnexion.Designer.cs
@@ -29,82 +29,89 @@ namespace Fumoblilite.Interface.Forms
             // lblTitre
             // 
             this.lblTitre.AutoSize = true;
-            this.lblTitre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
-            this.lblTitre.Location = new System.Drawing.Point(50, 30);
+            this.lblTitre.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTitre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.lblTitre.Location = new System.Drawing.Point(67, 37);
+            this.lblTitre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitre.Name = "lblTitre";
-            this.lblTitre.Size = new System.Drawing.Size(326, 24);
+            this.lblTitre.Size = new System.Drawing.Size(400, 32);
             this.lblTitre.TabIndex = 0;
             this.lblTitre.Text = "Gestion de Transport en Commun";
             this.lblTitre.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.lblTitre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
             // 
             // lblNomUtilisateur
             // 
             this.lblNomUtilisateur.AutoSize = true;
-            this.lblNomUtilisateur.Location = new System.Drawing.Point(50, 80);
+            this.lblNomUtilisateur.Location = new System.Drawing.Point(67, 98);
+            this.lblNomUtilisateur.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNomUtilisateur.Name = "lblNomUtilisateur";
-            this.lblNomUtilisateur.Size = new System.Drawing.Size(87, 13);
+            this.lblNomUtilisateur.Size = new System.Drawing.Size(109, 16);
             this.lblNomUtilisateur.TabIndex = 1;
-            this.lblNomUtilisateur.Text = "Nom d'utilisateur:";
+            this.lblNomUtilisateur.Text = "Nom d\'utilisateur:";
             // 
             // lblMotDePasse
             // 
             this.lblMotDePasse.AutoSize = true;
-            this.lblMotDePasse.Location = new System.Drawing.Point(50, 120);
+            this.lblMotDePasse.Location = new System.Drawing.Point(67, 148);
+            this.lblMotDePasse.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMotDePasse.Name = "lblMotDePasse";
-            this.lblMotDePasse.Size = new System.Drawing.Size(74, 13);
+            this.lblMotDePasse.Size = new System.Drawing.Size(92, 16);
             this.lblMotDePasse.TabIndex = 2;
             this.lblMotDePasse.Text = "Mot de passe:";
             // 
             // txtNomUtilisateur
             // 
-            this.txtNomUtilisateur.Location = new System.Drawing.Point(150, 80);
+            this.txtNomUtilisateur.Location = new System.Drawing.Point(200, 98);
+            this.txtNomUtilisateur.Margin = new System.Windows.Forms.Padding(4);
             this.txtNomUtilisateur.Name = "txtNomUtilisateur";
-            this.txtNomUtilisateur.Size = new System.Drawing.Size(200, 20);
+            this.txtNomUtilisateur.Size = new System.Drawing.Size(265, 22);
             this.txtNomUtilisateur.TabIndex = 3;
             // 
             // txtMotDePasse
             // 
-            this.txtMotDePasse.Location = new System.Drawing.Point(150, 120);
+            this.txtMotDePasse.Location = new System.Drawing.Point(200, 148);
+            this.txtMotDePasse.Margin = new System.Windows.Forms.Padding(4);
             this.txtMotDePasse.Name = "txtMotDePasse";
             this.txtMotDePasse.PasswordChar = '*';
-            this.txtMotDePasse.Size = new System.Drawing.Size(200, 20);
+            this.txtMotDePasse.Size = new System.Drawing.Size(265, 22);
             this.txtMotDePasse.TabIndex = 4;
             // 
             // btnConnexion
             // 
-            this.btnConnexion.Location = new System.Drawing.Point(150, 160);
-            this.btnConnexion.Name = "btnConnexion";
-            this.btnConnexion.Size = new System.Drawing.Size(90, 30);
-            this.btnConnexion.TabIndex = 5;
-            this.btnConnexion.Text = "Connexion";
-            this.btnConnexion.UseVisualStyleBackColor = true;
-            this.btnConnexion.Click += new System.EventHandler(this.btnConnexion_Click);
             this.btnConnexion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
             this.btnConnexion.FlatAppearance.BorderSize = 0;
             this.btnConnexion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnConnexion.ForeColor = System.Drawing.Color.White;
+            this.btnConnexion.Location = new System.Drawing.Point(113, 197);
+            this.btnConnexion.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConnexion.Name = "btnConnexion";
+            this.btnConnexion.Size = new System.Drawing.Size(120, 37);
+            this.btnConnexion.TabIndex = 5;
+            this.btnConnexion.Text = "Connexion";
+            this.btnConnexion.UseVisualStyleBackColor = false;
+            this.btnConnexion.Click += new System.EventHandler(this.btnConnexion_Click);
             // 
             // btnRetour
             // 
-            this.btnRetour.Location = new System.Drawing.Point(260, 160);
-            this.btnRetour.Name = "btnRetour";
-            this.btnRetour.Size = new System.Drawing.Size(90, 30);
-            this.btnRetour.TabIndex = 6;
-            this.btnRetour.Text = "Retour";
-            this.btnRetour.UseVisualStyleBackColor = true;
-            this.btnRetour.Click += new System.EventHandler(this.btnRetour_Click);
             this.btnRetour.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
             this.btnRetour.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
             this.btnRetour.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRetour.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.btnRetour.Location = new System.Drawing.Point(265, 197);
+            this.btnRetour.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRetour.Name = "btnRetour";
+            this.btnRetour.Size = new System.Drawing.Size(120, 37);
+            this.btnRetour.TabIndex = 6;
+            this.btnRetour.Text = "Retour";
+            this.btnRetour.UseVisualStyleBackColor = false;
+            this.btnRetour.Click += new System.EventHandler(this.btnRetour_Click);
             // 
             // FormConnexion
             // 
             this.AcceptButton = this.btnConnexion;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 220);
+            this.ClientSize = new System.Drawing.Size(533, 271);
             this.Controls.Add(this.btnRetour);
             this.Controls.Add(this.btnConnexion);
             this.Controls.Add(this.txtMotDePasse);
@@ -114,6 +121,7 @@ namespace Fumoblilite.Interface.Forms
             this.Controls.Add(this.lblTitre);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormConnexion";
--- Fumoblilite.Systeme/Services/ServiceLigne.cs
+++ Fumoblilite.Systeme/Services/ServiceLigne.cs
@@ -5,22 +5,47 @@ using Fumoblilite.Systeme.Interfaces;
 
 namespace Fumoblilite.Systeme.Services
 {
+    /// <summary>
+    /// Service pour la gestion des lignes et de leurs arrêts.
+    /// </summary>
     public class ServiceLigne
     {
+        /// <summary>
+        /// Référentiel pour les lignes.
+        /// </summary>
         private readonly IRepositoryLigne _repositoryLigne;
+
+        /// <summary>
+        /// Référentiel pour les arrêts de ligne.
+        /// </summary>
         private readonly IRepositoryArretLigne _repositoryArretLigne;
 
+        /// <summary>
+        /// Initialise une nouvelle instance du service de gestion des lignes.
+        /// </summary>
+        /// <param name="repositoryLigne">Référentiel des lignes.</param>
+        /// <param name="repositoryArretLigne">Référentiel des arrêts de ligne.</param>
         public ServiceLigne(IRepositoryLigne repositoryLigne, IRepositoryArretLigne repositoryArretLigne)
         {
             _repositoryLigne = repositoryLigne;
             _repositoryArretLigne = repositoryArretLigne;
         }
 
+        /// <summary>
+        /// Récupère toutes les lignes.
+        /// </summary>
+        /// <returns>Liste de toutes les lignes.</returns>
         public List<Ligne> ObtenirToutes()
         {
             return _repositoryLigne.ObtenirToutes();
         }
 
+        /// <summary>
+        /// Récupère une ligne par son identifiant, avec option d'inclure les arrêts.
+        /// </summary>
+        /// <param name="id">Identifiant de la ligne.</param>
+        /// <param name="inclureArrets">Inclure les arrêts de la ligne.</param>
+        /// <returns>La ligne correspondante ou null si non trouvée.</returns>
         public Ligne ObtenirParId(int id, bool inclureArrets = false)
         {
             var ligne = _repositoryLigne.ObtenirParId(id);
@@ -31,6 +56,12 @@ namespace Fumoblilite.Systeme.Services
             return ligne;
         }
 
+        /// <summary>
+        /// Ajoute une nouvelle ligne.
+        /// </summary>
+        /// <param name="ligne">Ligne à ajouter.</param>
+        /// <returns>Identifiant de la ligne ajoutée.</returns>
+        /// <exception cref="ArgumentException">Si le numéro ou le nom est manquant.</exception>
         public int Ajouter(Ligne ligne)
         {
             if (string.IsNullOrEmpty(ligne.Numero) || string.IsNullOrEmpty(ligne.Nom))
@@ -39,6 +70,12 @@ namespace Fumoblilite.Systeme.Services
             return _repositoryLigne.Ajouter(ligne);
         }
 
+        /// <summary>
+        /// Modifie une ligne existante.
+        /// </summary>
+        /// <param name="ligne">Ligne à modifier.</param>
+        /// <returns>True si la modification a réussi, sinon false.</returns>
+        /// <exception cref="ArgumentException">Si le numéro ou le nom est manquant.</exception>
         public bool Modifier(Ligne ligne)
         {
             if (string.IsNullOrEmpty(ligne.Numero) || string.IsNullOrEmpty(ligne.Nom))
@@ -48,26 +85,45 @@ namespace Fumoblilite.Systeme.Services
             return _repositoryLigne.Modifier(ligne);
         }
 
+        /// <summary>
+        /// Supprime une ligne par son identifiant.
+        /// </summary>
+        /// <param name="id">Identifiant de la ligne à supprimer.</param>
+        /// <returns>True si la suppression a réussi, sinon false.</returns>
         public bool Supprimer(int id)
         {
             return _repositoryLigne.Supprimer(id);
         }
 
+        /// <summary>
+        /// Ajoute un arrêt à une ligne.
+        /// </summary>
+        /// <param name="arretLigne">Arrêt de ligne à ajouter.</param>
+        /// <returns>True si l'ajout a réussi, sinon false.</returns>
         public bool AjouterArret(ArretLigne arretLigne)
         {
             return _repositoryArretLigne.Ajouter(arretLigne) > 0;
         }
 
+        /// <summary>
+        /// Modifie un arrêt de ligne existant.
+        /// </summary>
+        /// <param name="arretLigne">Arrêt de ligne à modifier.</param>
+        /// <returns>True si la modification a réussi, sinon false.</returns>
         public bool ModifierArret(ArretLigne arretLigne)
         {
             arretLigne.DateModification = DateTime.Now;
             return _repositoryArretLigne.Modifier(arretLigne);
         }
 
+        /// <summary>
+        /// Supprime un arrêt de ligne par son identifiant.
+        /// </summary>
+        /// <param name="id">Identifiant de l'arrêt de ligne à supprimer.</param>
+        /// <returns>True si la suppression a réussi, sinon false.</returns>
         public bool SupprimerArret(int id)
         {
             return _repositoryArretLigne.Supprimer(id);
         }
     }
 }
-
